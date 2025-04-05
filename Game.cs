557454b98358bcd372228e5f5da5cec628d19bb5
MarceloCamachh <marcelocamachogutierrez3@gmail.com>
2025-04-05 19:25:07 -0400

    clase objeto

--- Game.cs
+++ Game.cs
@@ -1,15 +1,26 @@
-﻿using OpenTK;
+﻿using LetraU;
+using OpenTK;
 using OpenTK.Graphics;
 using OpenTK.Graphics.OpenGL;
 using OpenTK.Input;
 using System;
+using System.Collections.Generic;
 
 namespace TareaU
 {
     class Game : GameWindow
     {
+        public Objeto objeto;
+        public Objeto objeto2;
+
         public Game(int width, int height) : base(width, height, GraphicsMode.Default, "Tarea Letra U ")
         {
+            objeto = new Objeto(new Vector3(1, 0, 0), this.getVertices());
+            objeto2 = new Objeto(new Vector3(-1, 0, 0), this.getVertices());
+
+
+            Console.WriteLine(this.getVertices().Count);
+            Console.WriteLine("hola");
         }
 
         protected override void OnLoad(EventArgs e)
@@ -49,12 +60,14 @@ namespace TareaU
             GL.LoadMatrix(ref modelview);
 
             // Dibuja múltiples letras U (ahora sin Translate)
-            DibujarLetraU(new Vector3(-2f, 0.0f, 0.0f));  // Centro
+            //DibujarLetraU(new Vector3(-2f, 0.0f, 0.0f));  // Centro
             //DibujarLetraU(new Vector3(1.5f, 0.0f, 0.0f)); // A la derecha
             //DibujarLetraU(new Vector3(-1.5f, 0.0f, 0.0f)); // A la izquierda
+            this.objeto.Draw();
+            //this.objeto2.Draw();
+
 
             DibujarEjes(); // Solo una vez
-
             SwapBuffers();
         }
 
@@ -65,46 +78,53 @@ namespace TareaU
             GL.Scale(0.3f, 0.3f, 0.3f); // Puedes ajustar este valor para cambiar el tamaño de todas las letras
             GL.Color4(0.5f, 0.5f, 0.5f, 1.0f);
 
-            // Parte lateral izquierda de la U
-            DibujaCaja(posicion, -0.8f, -0.8f, 0.3f, -0.4f, 1.2f, -0.3f);
-            // Parte lateral derecha de la U
-            DibujaCaja(posicion, 0.4f, -0.8f, 0.3f, 0.8f, 1.2f, -0.3f);
-            // Parte inferior de la U
-            DibujaCaja(posicion, -0.8f, -1.2f, 0.3f, 0.8f, -0.8f, -0.3f);
+            
 
             GL.PopMatrix();
         }
 
-        private void DibujaCaja(Vector3 offset, float x1, float y1, float z1, float x2, float y2, float z2)
+        public List<Vector3 []> getVertices()
+        {
+            List<Vector3[]> list = new List<Vector3[]>();
+            // Parte lateral izquierda de la U
+            DibujaCaja(list, -0.8f, -0.8f, 0.3f, -0.4f, 1.2f, -0.3f);
+            // Parte lateral derecha de la U
+            DibujaCaja(list, 0.4f, -0.8f, 0.3f, 0.8f, 1.2f, -0.3f);
+            // Parte inferior de la U
+            DibujaCaja(list, -0.8f, -1.2f, 0.3f, 0.8f, -0.8f, -0.3f);
+            return list;
+        } 
+
+        private void DibujaCaja(List<Vector3[]> list, float x1, float y1, float z1, float x2, float y2, float z2)
         {
             // 8 vértices con offset aplicado
             Vector3[] v = new Vector3[8];
-            v[0] = new Vector3(x1, y2, z1) + offset;
-            v[1] = new Vector3(x2, y2, z1) + offset;
-            v[2] = new Vector3(x2, y1, z1) + offset;
-            v[3] = new Vector3(x1, y1, z1) + offset;
-            v[4] = new Vector3(x1, y2, z2) + offset;
-            v[5] = new Vector3(x2, y2, z2) + offset;
-            v[6] = new Vector3(x2, y1, z2) + offset;
-            v[7] = new Vector3(x1, y1, z2) + offset;
+            v[0] = new Vector3(x1, y2, z1);
+            v[1] = new Vector3(x2, y2, z1);
+            v[2] = new Vector3(x2, y1, z1);
+            v[3] = new Vector3(x1, y1, z1);
+            v[4] = new Vector3(x1, y2, z2);
+            v[5] = new Vector3(x2, y2, z2);
+            v[6] = new Vector3(x2, y1, z2);
+            v[7] = new Vector3(x1, y1, z2);
 
             // Caras (6)
-            DibujarCara(v[0], v[1], v[2], v[3]); // Frente
-            DibujarCara(v[4], v[5], v[6], v[7]); // Atrás
-            DibujarCara(v[0], v[4], v[7], v[3]); // Izquierda
-            DibujarCara(v[1], v[5], v[6], v[2]); // Derecha
-            DibujarCara(v[0], v[1], v[5], v[4]); // Superior
-            DibujarCara(v[3], v[2], v[6], v[7]); // Inferior
+            list.Add(DibujarCara(v[0], v[1], v[2], v[3])); // Frente
+            list.Add(DibujarCara(v[4], v[5], v[6], v[7])); // Atrás
+            list.Add(DibujarCara(v[0], v[4], v[7], v[3])); // Izquierda
+            list.Add(DibujarCara(v[1], v[5], v[6], v[2])); // Derecha
+            list.Add(DibujarCara(v[0], v[1], v[5], v[4])); // Superior
+            list.Add(DibujarCara(v[3], v[2], v[6], v[7])); // Inferior
         }
 
-        private void DibujarCara(Vector3 a, Vector3 b, Vector3 c, Vector3 d)
+        private Vector3[] DibujarCara(Vector3 a, Vector3 b, Vector3 c, Vector3 d)
         {
-            GL.Begin(PrimitiveType.LineLoop);
-            GL.Vertex3(a);
-            GL.Vertex3(b);
-            GL.Vertex3(c);
-            GL.Vertex3(d);
-            GL.End();
+            Vector3[] array = new Vector3[4];
+            array[3] = d;
+            array[1] = b;
+            array[2] = c;
+            array[0] = a;
+            return array;
         }
 
         private void DibujarEjes()
---
+++ Objeto.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+namespace LetraU
+{
+    public class Objeto
+    {
+        private Vector3 centro;
+        private List<Vector3[]> listaDeVertices;
+
+        public Objeto(Vector3 centro,List<Vector3[]> list)
+        {
+            this.centro = centro;
+            this.listaDeVertices = list;
+        }
+
+        public void Draw()
+        {
+            GL.PushMatrix();
+            //GL.Rotate(rotacionY, 0f, 1f, 0f);
+            GL.Scale(0.4f, 0.4f, 0.4f); // Puedes ajustar este valor para cambiar el tamaño de todas las letras
+            GL.Color4(0.5f, 0.5f, 0.5f, 1.0f);
+            GL.Begin(PrimitiveType.Quads);
+
+            foreach (Vector3[] vector in this.listaDeVertices)
+            {
+                GL.Vertex3(vector[0]+this.centro);
+                GL.Vertex3(vector[1]+this.centro);
+                GL.Vertex3(vector[2]+this.centro);
+                GL.Vertex3(vector[3]+this.centro);
+            }
+
+            GL.End();
+            GL.PopMatrix();
+        }
+    }
+}
